f8c0e0fd9c5729a61ff1029c238103b445d0703e
Rafael Pereira Dias <br.rafaeldias@gmail.com>
2022-09-12 14:53:01 -0300

    adicionado as data annotations para criação das migrações e do banco de dados

--- LanchesMac/Models/Lanche.cs
+++ LanchesMac/Models/Lanche.cs
@@ -1,15 +1,51 @@
-﻿namespace LanchesMac.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LanchesMac.Models
 {
+    [Table("Lanches")]
     public class Lanche
     {
+        [Key]
         public int LancheId { get; set; }
+
+        [Required(ErrorMessage = "O nome do lanche deve ser informado")]
+        [Display(Name = "Nome do Lanche")]
+        [StringLength(80, MinimumLength = 10, ErrorMessage = "O {0} deve ter no mínimo {1} e no máximo {2} caracteres")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "A descrição do lanche deve ser informada")]
+        [Display(Name = "Descrição do Lanche")]
+        [MinLength(20, ErrorMessage = "Descrição deve ter no mínimo {1} caracteres")]
+        [MaxLength(200, ErrorMessage = "Descição não pode exceder {1} caracteres")]
         public string DescricaoCurta { get; set; }
+
+        //[NotMapped]
+        //public DateTime DataDeCriacao { get; set; }
+
+        [Required(ErrorMessage = "A descrição detalhada do lanche deve ser informada")]
+        [Display(Name = "Descrição detalhada do Lanche")]
+        [StringLength(200, MinimumLength = 20, ErrorMessage = "A {0} deve ter no mínimo {1} e no máximo {2} caracteres")]
         public string DescricaoDetalhada { get; set; }
+
+        [Required(ErrorMessage = "Inform o preço do Lanche")]
+        [Display(Name = "Preço")]
+        [Column(TypeName = "decimal(10,2)")]
+        [Range(1,999.99, ErrorMessage = "O {0} deve estar entre 1 e 999,99")]
         public decimal Preco { get; set; }
+
+        [Display(Name = "Caminho da Imagem normal")]
+        [StringLength(200, ErrorMessage = "O {0} deve ter no máximo {1} caracteres")]
         public string ImagemUrl { get; set; }
+
+        [Display(Name = "Caminho da Imagem Miniatura")]
+        [StringLength(200, ErrorMessage = "O {0} deve ter no máximo {1} caracteres")]
         public string ImagemThumbnailUrl { get; set; }
+
+        [Display(Name = "Prefereido?")]
         public bool IsLanchePreferido { get; set; }
+
+        [Display(Name = "Estoque")]
         public bool EmEstoque { get; set; }
 
         public int CategoriaId { get; set; }
