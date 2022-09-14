65ac5a7814e001eb1a79e77c40dab75423f4357e
Rafael Pereira Dias <br.rafaeldias@gmail.com>
2022-09-14 12:51:55 -0300

    registrado CarrinhoCompra (model) como um serviço scoped, que será gerado um novo a cada requisição.

--- LanchesMac/Startup.cs
+++ LanchesMac/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using LanchesMac.Context;
+using LanchesMac.Models;
 using LanchesMac.Repositories;
 using LanchesMac.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
@@ -21,7 +22,7 @@ public class Startup
         
         services.AddTransient<ILancheRepository, LancheRepository>();
         services.AddTransient<ICategoriaRepository, CategoriaRepository>();
-
+        services.AddScoped(sp => CarrinhoCompra.GetCarrinho(sp));
         services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
         services.AddControllersWithViews();
